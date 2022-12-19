289cf8e9adad96725644363b27a9802ce8521154
morelup <matthew.orelup@gmail.com>
2022-12-19 00:38:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ var secretName = 'projects/'+process.env.GOOGLE_CLOUD_PROJECT+"/secrets/Firebase
   // WARNING: Do not print the secret in a production environment - this
   // snippet is showing how to access the secret material.
   firebase.initializeApp(payload);
-  firebase.database().ref('nodelog').on('child_changed', (snapshot) => {
+  firebase.database().ref('nodelog').on('child_changed/December_2022', (snapshot) => {
 	console.log(snapshot.val());
 	}, (errorObject) => {
 	  console.log('The read failed: ' + errorObject.name);
