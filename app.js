75fb1f9df666ca140334bcb1db12bbfabfd4da45
morelup <matthew.orelup@gmail.com>
2022-12-18 23:45:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ const app = express();
 const server = require('http').Server(app);
 const url = require('url');
 const fetch = require('node-fetch');
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Firestore.
 const firebase = require('firebase-admin');
