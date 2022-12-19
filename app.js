a9e4397efd60ffdf23db696beaae62e81898e06c
morelup <matthew.orelup@gmail.com>
2022-12-19 00:13:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -47,10 +47,7 @@ var secretName = 'projects/'+process.env.GOOGLE_CLOUD_PROJECT+"/secrets/Firebase
   // WARNING: Do not print the secret in a production environment - this
   // snippet is showing how to access the secret material.
   firebase.initializeApp(payload);
-  DatabaseReference ref = FirebaseDatabase
-		.getInstance()
-		.getReference("/public_resource");
-	firebase.database().ref('/nodelog').on('child_added', function(postSnapshot) {
+  firebase.database().ref('/nodelog').on('child_added', function(postSnapshot) {
 			System.out.println(postSnapshot);
 		}
 	);
