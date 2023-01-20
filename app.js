220f13a801ca96c202fb0ef24de6c7d178e8917f
morelup <matthew.orelup@gmail.com>
2023-01-19 22:47:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -298,8 +298,7 @@ function boardInfo(msg){
 			'Authorization': mondayAuthKey
 		  },
 		  body: body,
-		})
-	  .then(res) => {
+		}).then(res) => {
 			io.to(msg.channel).emit('boardData',res)
 			var subtaskInfo = JSON.parse(res.boards[0].columns[1].settings_str);
 			
