ffd2bb03e571160d6ef644b635c2b89b6e9a0bca
morelup <matthew.orelup@gmail.com>
2023-07-20 12:16:17 -0400

    Update config-Defect.js

---
+++ public/js/config-Defect.js
@@ -0,0 +1,10 @@
+const studio_images{
+	"prompt":"https://us7.studioportal.io/icons/prompt.svg",
+	"hangup":"https://us7.studioportal.io/icons/hangup.svg",
+	"integration":"https://us7.studioportal.io/icons/integration.svg",
+	"system":"https://us7.studioportal.io/icons/data_store_system.svg",
+	"prompt_record":"https://us7.studioportal.io/icons/prompt_recorder.svg",
+	"webrtc":"https://us7.studioportal.io/img/icon-webrtc.d78a159a.svg"
+	
+	
+}
